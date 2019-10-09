edddf38d9cebfbdfbcdb07e67f2c54897bd024c0
Kris Ringler <kmringler@gmail.com>
2019-10-08 18:18:16 -0700

    can have multiple operators in both functionality

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -18,28 +18,46 @@ function numberButtonHandler(event) {
 	updateDisplay();
 }
 
-function decimalHandler(event) {}
+function decimalHandler(event) {
+	var inputtedDecimal = "";
+	inputtedDecimal = $(event.currentTarget);
+	inputtedDecimal = inputtedDecimal.find("p").text();
+	displayArray.push(inputtedDecimal);
+	stringNumberToPush += inputtedDecimal;
+
+	updateDisplay();
+}
 
 function operatorButtonHandler(event) {
 	var inputtedOperator = "";
 	inputtedOperator = $(event.currentTarget);
 	inputtedOperator = inputtedOperator.find("p").text();
-	displayArray.push(inputtedOperator);
-	calculationArray.push(stringNumberToPush);
-	calculationArray.push(inputtedOperator);
-	stringNumberToPush = "";
-	updateDisplay();
+	// displayArray.push(inputtedOperator);
+	// calculationArray.push(stringNumberToPush);
+	// calculationArray.push(inputtedOperator);
+	// stringNumberToPush = "";
+	// updateDisplay();
+	var lastInput = "";
+	lastInput = displayArray[displayArray.length - 1];
+	if (isAnOp(lastInput)) {
+		calculationArray.pop();
+		calculationArray.push(inputtedOperator);
+		displayArray.push(inputtedOperator);
+		updateDisplay();
+	} else {
+		displayArray.push(inputtedOperator);
+		calculationArray.push(stringNumberToPush);
+		calculationArray.push(inputtedOperator);
+		stringNumberToPush = "";
+		updateDisplay();
+	}
 }
 
 function equalsButtonHandler(event) {
 	calculationArray.push(stringNumberToPush);
 	stringNumberToPush = "";
 	displayArray = [];
-	var answer = calculate(
-		calculationArray[0],
-		calculationArray[2],
-		calculationArray[1]
-	);
+	var answer = multipleOperators();
 	displayArray.push(answer);
 	updateDisplay();
 }
@@ -83,4 +101,29 @@ function allClear() {
 	displayArray = [];
 	calculationArray = [];
 	updateDisplay();
+}
+
+function multipleOperators() {
+	var number1 = calculationArray[0];
+	var operator = calculationArray[1];
+	var number2 = calculationArray[2];
+	var answer = calculate(number1, number2, operator);
+	console.log(answer);
+	calculationArray = calculationArray.slice(3);
+	while (calculationArray.length >= 2) {
+		answer = calculate(answer, calculationArray[1], calculationArray[0]);
+		calculationArray = calculationArray.slice(2);
+	}
+	return answer;
+}
+
+function isAnOp(lastInput) {
+	if (
+		lastInput === "+" ||
+		lastInput === "-" ||
+		lastInput === "*" ||
+		lastInput === "/"
+	) {
+		return true;
+	}
 }
